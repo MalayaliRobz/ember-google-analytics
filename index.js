7893e5caff8ed5b2826d32a0148e3722ca4fe601
Robin Philip Thomas <robinphilipthomas@outlook.in>
2020-10-31 22:31:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ module.exports = {
   name: require('./package').name,
 
   contentFor(type) {
-    if (type === 'body-footer') {
+    if (type === 'head-footer') {
       return `
-        <script async src="https://www.googletagmanager.com/gtag/js"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138326907-2"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
